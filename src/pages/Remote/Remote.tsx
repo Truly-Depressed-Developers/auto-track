c03f33c4b999e5d31e3f66d511b149563b1b5b97
MBrosik <michal.bros1@gmail.com>
2023-04-22 21:49:41 +0200

    Auto stash before checking out "HEAD"

--- src/pages/Remote/Remote.tsx
+++ src/pages/Remote/Remote.tsx
@@ -4,12 +4,12 @@ import { Link, Outlet } from 'react-router-dom';
 
 function Remote() {
     return(
-        <div id="remote">
-            <Link to="state">State</Link>
+        <div id="remote" style={{padding: "20px"}}>
+            {/* <Link to="state">State</Link>
             <Link to="location">Location</Link>
             <Link to="actions">Actions</Link>
             <Link to="login">Login</Link>
-            <Link to="register">Register</Link>
+            <Link to="register">Register</Link> */}
             <Outlet />
         </div>
     )
